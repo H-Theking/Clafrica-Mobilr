be2432132e84026c5f2b9c28e1ee6530bf0cd6a4
H-Theking <princeme.sama@gmail.com>
2020-03-21 23:12:49 +0100

    Added help and about pages in action bar menu

--- app/src/main/java/org/resulam/www/clafricamobile/OpenSettings.java
+++ app/src/main/java/org/resulam/www/clafricamobile/OpenSettings.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
-import com.gc.materialdesign.views.ButtonRectangle;
+import org.resulam.www.clafricamobile.menus.About;
+import org.resulam.www.clafricamobile.menus.Help;
 
 public class OpenSettings extends AppCompatActivity {
 
@@ -15,6 +19,27 @@ public class OpenSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.help:
+                showHelp();
+                return true;
+            case R.id.about:
+                showAboutPage();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void openLanguageSettings(View view){
@@ -25,5 +50,15 @@ public class OpenSettings extends AppCompatActivity {
     public void showAllKeyboards(View view){
         InputMethodManager imeManager = (InputMethodManager) getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
         imeManager.showInputMethodPicker();
+    }
+
+    public void showHelp() {
+        Intent helpIntent = new Intent(this, Help.class);
+        startActivity(helpIntent);
+    }
+
+    public void showAboutPage() {
+        Intent aboutIntent = new Intent(this, About.class);
+        startActivity(aboutIntent);
     }
 }
